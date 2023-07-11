082b97f43c67589b9cc14a7df75d89b3a6e5c6dc
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-07-11 23:22:32 +0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+--  If you wish to get an key dm pvhak on discord or jailedd | price: 5-20 robux
+
+local key = "" -- Enter Key Here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/ClearWare/main/Key_System.lua"))()
