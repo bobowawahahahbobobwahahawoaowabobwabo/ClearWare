0275460489fdfcf73f20fc5591d18abc41f7c1f8
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-07-13 00:23:34 +0300

    Update Game_Loader.lua

--- Game_Loader.lua
+++ Game_Loader.lua
@@ -1,4 +1,5 @@
-local j = { fsim = 2866967438, rmavatar = 6708206173, } -- Adding more games soon | This obfuscation is to keep the mobile skiddies out
+local j = { fsim = 2866967438, rmavatar = 6708206173, fyweather = 12507488315, } -- Adding more games soon | This obfuscation is to keep the mobile skiddies out
 
 if game.PlaceId == j.fsim then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\54\88\90\83\117\103\66\54"))() end
 if game.PlaceId == j.rmavatar then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\48\121\100\90\51\56\70\110"))() end
+if game.PlaceId == j.fyweather then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\104\52\119\87\86\48\89\48\10"))() end
