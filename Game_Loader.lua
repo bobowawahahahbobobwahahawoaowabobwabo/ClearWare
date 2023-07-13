864ac1d46bd3d25f88178092c88b640fd59736ea
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-07-13 14:54:35 +0300

    Update Game_Loader.lua

--- Game_Loader.lua
+++ Game_Loader.lua
@@ -1,4 +1,4 @@
-local s = { fsim = 2866967438, rmavatar = 6708206173, fyweather = 12507488315, }  -- Adding more games soon | This obfuscation is to keep the mobile skiddies out
-if game.PlaceId == s.fsim then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\54\88\90\83\117\103\66\54"))() end
-if game.PlaceId == s.rmavatar then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\48\121\100\90\51\56\70\110"))() end
-if game.PlaceId == s.fyweather then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\104\52\119\87\86\48\89\48"))() end
+local games = { fsim = 2866967438, rmavatar = 6708206173, fyweather = 12507488315, } 
+if game.PlaceId == games.fsim then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\54\88\90\83\117\103\66\54"))() end
+if game.PlaceId == games.rmavatar then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\48\121\100\90\51\56\70\110"))() end
+if game.PlaceId == games.fyweather then loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\104\52\119\87\86\48\89\48"))() end
