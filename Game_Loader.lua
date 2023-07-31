017ca91ada91661343ae837cc860fc018dbde241
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-07-31 19:19:02 +0300

    Update Game_Loader.lua

--- Game_Loader.lua
+++ Game_Loader.lua
@@ -1,4 +1,5 @@
-local games = { fsim = 2866967438, rmavatar = 6708206173, fyweather = 12507488315, } 
+local games = { fsim = 2866967438, rmavatar = 6708206173, fyweather = 12507488315, kat = 621129760 } 
 if game.PlaceId == games.fsim then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Fishing%20Simulator.txt"))() end
 if game.PlaceId == games.rmavatar then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Rate%20my%20avatar.txt"))() end
 if game.PlaceId == games.fyweather then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Flex%20Your%20Weather"))() end
+if game.PlaceId == games.kat then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Kat"))() end
