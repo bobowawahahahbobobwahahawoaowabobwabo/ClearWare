34abb06b7171e95c6968eb5ad5a9a714379395bb
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-08-03 19:51:16 +0300

    Update Game_Loader.lua

--- Game_Loader.lua
+++ Game_Loader.lua
@@ -1,5 +1,16 @@
-local games = { fsim = 2866967438, rmavatar = 6708206173, fyweather = 12507488315, kat = 621129760 } 
-if game.PlaceId == games.fsim then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Fishing%20Simulator.txt"))() end
-if game.PlaceId == games.rmavatar then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Rate%20my%20avatar.txt"))() end
-if game.PlaceId == games.fyweather then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Flex%20Your%20Weather"))() end
-if game.PlaceId == games.kat then loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Kat"))() end
+local games = {
+    fsim = 2866967438,
+    rmavatar = 6708206173,
+    fyweather = 12507488315,
+    kat = 621129760
+}
+
+if game.PlaceId == games.fsim then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Fishing%20Simulator.txt"))()
+elseif game.PlaceId == games.rmavatar then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Rate%20my%20avatar.txt"))()
+elseif game.PlaceId == games.fyweather then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Flex%20Your%20Weather.txt"))()
+elseif game.PlaceId == games.kat then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/knifeabilitytest"))()
+end
