6da64bd4d87da9afc933ac3b44c1a3147977964e
Clear <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-09-01 15:40:10 +0300

    Update Game_Loader.lua

--- Game_Loader.lua
+++ Game_Loader.lua
@@ -10,7 +10,7 @@ if game.PlaceId == games.fsim then
 elseif game.PlaceId == games.rmavatar then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Rate%20my%20avatar.txt"))()
 elseif game.PlaceId == games.fyweather then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Flex%20Your%20Weather.txt"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/Flex%20Your%20Weather"))()
 elseif game.PlaceId == games.kat then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bobowawahahahbobobwahahawoaowabobwabo/Clearware/main/Clearware%20Games/knifeabilitytest"))()
 end
