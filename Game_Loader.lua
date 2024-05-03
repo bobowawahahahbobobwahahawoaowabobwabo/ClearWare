2b698bd8c3c6cbf7a2b5235fdceb8d8f13b77599
xyzzr <110189904+xyzzr@users.noreply.github.com>
2024-05-03 14:24:08 +0300

    Update Game_Loader.lua

--- Game_Loader.lua
+++ Game_Loader.lua
@@ -2,7 +2,8 @@ local games = {
     fsim = 2866967438,
     rmavatar = 6708206173,
     fyweather = 12507488315,
-    kat = 621129760
+    kat = 621129760,
+    mort = 4169490976
 }
 
 if game.PlaceId == games.fsim then
@@ -13,4 +14,6 @@ elseif game.PlaceId == games.fyweather then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzzr/Clearware/main/Clearware%20Games/Flex%20Your%20Weather.lua"))()
 elseif game.PlaceId == games.kat then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzzr/Clearware/main/Clearware%20Games/knifeabilitytest.lua"))()
+elseif game.PlaceId == games.mort then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzzr/Clearware/main/Clearware%20Games/mortem.lua"))()
 end
